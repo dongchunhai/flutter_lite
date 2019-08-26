f6b9c2a3db687c1289296999f1c66ea1c4f014e8
dongchunhai <dongchunhai@gotokeep.com>
2019-08-26 23:55:50 +0800

    add layout demo01

--- lib/main.dart
+++ lib/main.dart
@@ -4,145 +4,177 @@ import 'package:english_words/english_words.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // Widget titleSection = new Container(
-  //   padding: const EdgeInsets.all(32.0),
-  //   child: new Row(
-  //     children: [
-  //       new Expanded(
-  //         child: new Column(
-  //           crossAxisAlignment: CrossAxisAlignment.start,
-  //           children: [
-  //             new Container(
-  //               padding: const EdgeInsets.only(bottom: 8.0),
-  //               child: new Text(
-  //                 'Oeschinen Lake Campground',
-  //                 style: new TextStyle(
-  //                   fontWeight: FontWeight.bold,
-  //                 ),
-  //               ),
-  //             ),
-  //             new Text(
-  //               'Kandersteg, Switzerland',
-  //               style: new TextStyle(
-  //                 color: Colors.grey[500],
-  //               ),
-  //             ),
-  //           ],
-  //         ),
-  //       ),
-  //       new Icon(
-  //         Icons.star,
-  //         color: Colors.red[500],
-  //       ),
-  //       new Text('41'),
-  //     ],
-  //   ),
-  // );
   @override
   Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          Text('41'),
+        ],
+      ),
+    );
+
+    Column buildButtonColumn(IconData icon, String label) {
+      Color color = Theme.of(context).primaryColor;
+
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, color: color),
+          Container(
+            margin: const EdgeInsets.only(top: 8.0),
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: 12.0,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          buildButtonColumn(Icons.call, 'CALL'),
+          buildButtonColumn(Icons.near_me, 'ROUTE'),
+          buildButtonColumn(Icons.share, 'SHARE'),
+        ],
+      ),
+    );
+
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32.0),
+      child: Text(
+        '''
+        Welcome to the keep OA!
+        ''',
+        softWrap: true,
+      ),
+    );
+
+    Widget prodSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          new Image.asset(
+            'assets/images/carnival_03.jpg',
+            width: 137.0,
+            height: 220.5,
+            fit: BoxFit.cover,
+          ),
+          new Image.asset(
+            'assets/images/carnival_04.jpg',
+            width: 101.5,
+            height: 220.5,
+            fit: BoxFit.cover,
+          ),
+          new Image.asset(
+            'assets/images/carnival_05.jpg',
+            width: 136.5,
+            height: 220.5,
+            fit: BoxFit.cover,
+          ),
+        ],
+      ),
+    );
+
     return MaterialApp(
-      title: 'Startup Name Generator',
+      title: 'Flutter Demo xxx',
       theme: ThemeData(
         primaryColor: Colors.purple,
       ),
-      home: RandomWords(),
-    );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  createState() => new RandomWordsState();
-}
-
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _saved = Set<WordPair>();
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-
-  @override
-  Widget build(BuildContext context) {
-    // final wordPair = WordPair.random();
-    // return Text(wordPair.asPascalCase);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.list),
-            onPressed: _pushSaved,
-          )
-        ],
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  void _pushSaved() {
-    Navigator.of(context).push(MaterialPageRoute(
-      builder: (context) {
-        final tiles = _saved.map((pair) {
-          return ListTile(
-            title: Text(
-              pair.asPascalCase,
-              style: _biggerFont,
+      home: Scaffold(
+        appBar: AppBar(title: Text('卡路里狂欢节')),
+        body: ListView(
+          children: [
+            new Image.asset(
+              'assets/images/carnival_01.jpg',
+              width: 375.0,
+              height: 330.5,
+              fit: BoxFit.cover,
             ),
-          );
-        });
-        final divided = ListTile.divideTiles(
-          context: context,
-          tiles: tiles,
-        ).toList();
-
-        return Scaffold(
-          appBar: AppBar(
-            title: Text('Saved Suggestions'),
-          ),
-          body: ListView(
-            children: divided,
-          ),
-        );
-      },
-    ));
-  }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if (i.isOdd) return Divider();
-
-        final index = i ~/ 2;
-
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      },
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
-
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
+            new Image.asset(
+              'assets/images/carnival_02.jpg',
+              width: 375.0,
+              height: 158.5,
+              fit: BoxFit.cover,
+            ),
+            prodSection,
+            new Image.asset(
+              'assets/images/carnival_06.jpg',
+              width: 375.0,
+              height: 260.0,
+              fit: BoxFit.cover,
+            ),
+            new Image.asset(
+              'assets/images/carnival_07.jpg',
+              width: 375.0,
+              height: 213.0,
+              fit: BoxFit.cover,
+            ),
+            new Image.asset(
+              'assets/images/carnival_08.jpg',
+              width: 375.0,
+              height: 171.0,
+              fit: BoxFit.cover,
+            ),
+            new Image.asset(
+              'assets/images/carnival_09.jpg',
+              width: 375.0,
+              height: 191.5,
+              fit: BoxFit.cover,
+            ),
+            new Image.asset(
+              'assets/images/carnival_10.jpg',
+              width: 375.0,
+              height: 1550.0,
+              fit: BoxFit.cover,
+            ),
+            new Image.asset(
+              'assets/images/carnival_11.jpg',
+              width: 375.0,
+              height: 711.5,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
+        ),
       ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
     );
   }
 }
